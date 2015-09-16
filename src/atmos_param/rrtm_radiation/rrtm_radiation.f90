25f521556ffaec6dd8c512a6f68aa510e9aabb34
Martin Jucker <martin.jucker@gmail.com>
2015-09-16 11:21:35 -0400

    GPLv3 license

--- src/atmos_param/rrtm_radiation/rrtm_radiation.f90
+++ src/atmos_param/rrtm_radiation/rrtm_radiation.f90
@@ -2,8 +2,25 @@
 
       module rrtm_vars
 !
-!   Martin Jucker, 2015, https://github.com/mjucker/MiMA.
+!    Modeling an idealized Moist Atmosphere (MiMA)
+!    Copyright (C) 2015  Martin Jucker
+!    https://github.com/mjucker/MiMA
 !
+!    This program is free software: you can redistribute it and/or modify
+!    it under the terms of the GNU General Public License as published by
+!    the Free Software Foundation, either version 3 of the License, or 
+!    any later version.
+!
+!    This program is distributed in the hope that it will be useful,
+!    but WITHOUT ANY WARRANTY; without even the implied warranty of
+!    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!    GNU General Public License for more details.
+!
+!    You should have received a copy of the GNU General Public License
+!    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+!
+!
+!   RRTM_VARS:
 !   Contains all variables needed to 
 !   run the RRTM code, version for GCMs (hence the 'G'),
 !   other than astronomy, i.e. all variables needed
