870c34f145447191659022afa3de8edbc01e70ef
Martin Jucker <martin.jucker@gmail.com>
2016-03-10 18:51:58 -0500

    going back to RRTM defaults: set swhr=0 at TOA

--- src/atmos_param/rrtm_radiation/rrtmg_sw/gcm_model/src/rrtmg_sw_rad.nomcica.f90
+++ src/atmos_param/rrtm_radiation/rrtmg_sw/gcm_model/src/rrtmg_sw_rad.nomcica.f90
@@ -721,8 +721,9 @@
             swhrc(iplon,i) = (swnflxc(i+1) - swnflxc(i)) * zdpgcp
             swhr(iplon,i) = (swnflx(i+1) - swnflx(i)) * zdpgcp
          enddo
-         !mj swhrc(iplon,nlayers) = 0._rb ! AM2 radiation does not set the top layer to zero artificially
-         !mj swhr(iplon,nlayers) = 0._rb ! AM2 radiation does not set the top layer to zero artificially
+!mj RRTM sets top layer SW heating to zero. AM2 radiation does not do that
+         swhrc(iplon,nlayers) = 0._rb 
+         swhr(iplon,nlayers) = 0._rb 
 
 ! End longitude loop
       enddo
