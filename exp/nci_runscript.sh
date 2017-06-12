79c85f26d88befd9d3a0f760e2a588c5bf9f560b
Martin Jucker <martin.jucker@gmail.com>
2017-06-12 22:12:12 +0100

    stopping at error

--- exp/nci_runscript.sh
+++ exp/nci_runscript.sh
@@ -7,10 +7,18 @@ rundir=/path/to/run/directory
 npes=32
 num_executions=2
 
+# stop on error
+set -e
+
+# copy input files
 cp -r $path_to_inputfiles/* $rundir/
 
+# run the model
 cd $rundir
-mkdir RESTART
+if [ ! -d RESTART ]
+then
+  mkdir RESTART
+fi
 
 n=0
 while [ $n -lt $num_executions ]
@@ -20,5 +28,6 @@ do
   mppnccombine -r ${index}.atmos_daily.nc atmos_daily.nc.????
   mppnccombine -r ${index}.atmos_avg.nc atmos_avg.nc.????
   cp RESTART/*res* INPUT/
+  echo 'DONE WITH ITERATION '$n
   let n=$n+1
 done
